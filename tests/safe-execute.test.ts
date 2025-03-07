02686e8e63e68ac31d7b3d52146063fc09656501
Kei-K23 <keiksl2301@gmail.com>
2025-03-07 11:57:10 +0630

    chore: file structure

--- tests/safe-execute.test.ts
+++ tests/safe-execute.test.ts
@@ -1,44 +1,44 @@
-import { safeExecute } from "../src/index";
+import { safeExecute } from '../src/index';
 
-describe("safeExecute", () => {
-  it("should return result when function executes successfully", async () => {
-    const fn = async () => "Success";
+describe('safeExecute', () => {
+  it('should return result when function executes successfully', async () => {
+    const fn = async () => 'Success';
     const safeFn = safeExecute(fn);
     const [result, error] = await safeFn();
-    expect(result).toBe("Success");
+    expect(result).toBe('Success');
     expect(error).toBeNull();
   });
 
-  it("should return error when function throws", async () => {
+  it('should return error when function throws', async () => {
     const fn = async () => {
-      throw new Error("Failure");
+      throw new Error('Failure');
     };
     const safeFn = safeExecute(fn);
     const [result, error] = await safeFn();
     expect(result).toBeNull();
     expect(error).toBeInstanceOf(Error);
-    expect((error as Error).message).toBe("Failure");
+    expect((error as Error).message).toBe('Failure');
   });
 
-  it("should return fallback result when function failure", async () => {
+  it('should return fallback result when function failure', async () => {
     const fn = async () => {
-      throw new Error("Failure");
+      throw new Error('Failure');
     };
-    const fallbackFn = async () => "Fallback Success";
+    const fallbackFn = async () => 'Fallback Success';
     const safeFn = safeExecute(fn, {
       fallback: fallbackFn,
     });
     const [result, error] = await safeFn();
-    expect(result).toBe("Fallback Success");
+    expect(result).toBe('Fallback Success');
     expect(error).toBeNull();
   });
 
-  it("should return fallback error when fallback function throws", async () => {
+  it('should return fallback error when fallback function throws', async () => {
     const fn = async () => {
-      throw new Error("Failure");
+      throw new Error('Failure');
     };
     const fallbackFn = async () => {
-      throw new Error("Fallback Fn Failure");
+      throw new Error('Fallback Fn Failure');
     };
     const safeFn = safeExecute(fn, {
       fallback: fallbackFn,
@@ -46,6 +46,6 @@ describe("safeExecute", () => {
     const [result, error] = await safeFn();
     expect(result).toBeNull();
     expect(error).toBeInstanceOf(Error);
-    expect((error as Error).message).toBe("Fallback Fn Failure");
+    expect((error as Error).message).toBe('Fallback Fn Failure');
   });
 });
